97753c4d9f2805cbacd7cf357b7170104259c0b0
Alexandre Dubeau <adubeau@c2r4p3.42quebec.com>
2022-10-17 14:43:14 -0400

    push

--- log.h
+++ log.h
@@ -2,10 +2,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 #include <stdio.h>
 
 #define CLOG(log, ...) (log)->interface->info(log, __FILE__, __FUNCTION__, ft_itoa(__LINE__), __VA_ARGS__)
 #define ELOG(log, ...) (log)->interface->error(log, __FILE__, __FUNCTION__, ft_itoa(__LINE__), __VA_ARGS__)
+#define FLOG(log, ...) (log)->interface->file(log, __FILE__, __FUNCTION__, ft_itoa(__LINE__), __VA_ARGS__)
+#define RED		"\033[31m"
+#define END_COLOR     "\e[0m"
 
 typedef struct logger logger;
 
@@ -13,11 +17,13 @@ typedef struct log_interface {
 	int 	(*init)(logger *self, char *name);
 	void 	(*info)(struct logger *self, char *file, const char *function, char *line, ...);
 	void	(*error)(struct logger *self, char *file, const char *function, char *line, ...);
+	void	(*file)(struct logger *self, char *file, const char *function, char *line, ...);
 	void 	(*close)(logger *self);
 } log_interface;
 
 struct logger {
 	log_interface	*interface;
+	FILE 			*fd;
 	char *data;
 };
 
--- terminal.c
+++ terminal.c
@@ -2,7 +2,8 @@
 
 int init_terminal(logger *self, char *name)
 {
-	self->data = name;
+	self->data = strcat(name, "_log");
+	printf("\033[31m");
 	return 0;
 }
 
@@ -49,6 +50,30 @@ void error_terminal(struct logger *self, char *file, const char *function, char 
 	//fprintf(stderr, "On line %s in %s/%s :\n%s\n",line, function,file, msg);
 }
 
+void info_to_file(struct logger *self, char *file, const char *function, char *line, ...)
+{
+	va_list ap;
+	char 	*arg;
+	unsigned int i = 0;
+	unsigned int len;
+
+	self->fd = fopen(self->data, "w");
+	fprintf(self->fd, "On line %s in %s/%s :\n",line, function,file);
+	va_start(ap, line);
+	len = va_arg(ap, int);
+	while(i < len)
+	{
+		arg = va_arg(ap, char*);
+		fprintf(self->fd, "%s",arg);
+		i++;
+	}
+	if (len > 0)
+		fprintf(self->fd, "\n");
+	va_end(ap);
+
+}
+
+
 void close_terminal(logger *self)
 {
 
@@ -58,6 +83,7 @@ log_interface terminal = {
 		.init = init_terminal,
 		.info = info_terminal,
 		.error = error_terminal,
+		.file = info_to_file,
 		.close = close_terminal
 };
 
